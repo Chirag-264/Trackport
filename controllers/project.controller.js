8825a18a33cd7085f57340cbb6fe32f227b8266d
Chirag-264 <rchirag264@gmail.com>
2026-06-13 11:29:41 +0530

    added delete and update functionality for projects

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -78,4 +78,114 @@ async function getProject(req, res, next) {
     }
 }
 
-export {createProject, getProjects, getProject};
+async function deleteProject(req, res, next) {
+    try {
+        const projectId = req.params.id;
+
+        if(!mongoose.Types.ObjectId.isValid(projectId)) {
+            const error = new Error('Project ID is not valid');
+            error.statusCode = 400;
+            throw error;
+        }
+
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            const error = new Error("Project not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if(!project.userId.equals(req.user._id)) {
+            const error = new Error('Unauthorized');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        await project.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            data: project
+        });
+    }
+    catch(err) {
+        next(err);
+    }
+}
+
+async function updateProject(req, res, next) {
+    try {
+        const projectId = req.params.id;
+        const {title, description, status, githubUrl, liveUrl, techStack} = req.body;
+        
+        if(!mongoose.Types.ObjectId.isValid(projectId)) {
+            const error = new Error('Project ID is not valid');
+            error.statusCode = 400;
+            throw error;
+        }
+
+        const project = await Project.findById(projectId);
+
+        if(!project) {
+            const error = new Error('Project not found.');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if(!project.userId.equals(req.user._id)) {
+            const error = new Error('Forbidden');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        if (title !== undefined) {
+            const existingProject = await Project.findOne({
+                title,
+                userId: req.user._id,
+                _id: { $ne: projectId }
+            });
+
+            if (existingProject) {
+                const error = new Error("Project title already exists");
+                error.statusCode = 409;
+                throw error;
+            }
+
+            project.title = title;
+        }
+
+        if(description !== undefined) {
+            project.description = description;
+        }
+
+        if(status !== undefined) {
+            project.status = status;
+        }
+
+        if(githubUrl!== undefined) {
+            project.githubUrl = githubUrl;
+        }
+
+        if(liveUrl !== undefined) {
+            project.liveUrl = liveUrl;
+        }
+
+        if(techStack !== undefined) {
+            project.techStack = techStack;
+        }
+
+        await project.save();
+
+        res.status(200).json({
+            success: true,
+            data: project
+        })
+    }
+
+    catch(err) {
+        next(err);
+    }
+}
+
+export {createProject, getProjects, getProject, deleteProject, updateProject};
--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,6 +1,6 @@
 import Router from 'express';
 import { authUser } from '../middlewares/auth.middleware.js';
-import { createProject, getProjects, getProject } from '../controllers/project.controller.js';
+import { createProject, getProjects, getProject, deleteProject, updateProject } from '../controllers/project.controller.js';
 const projectRouter = Router();
 
 projectRouter.get('/projects', authUser, getProjects);
@@ -9,10 +9,8 @@ projectRouter.get('/projects/:id', authUser, getProject);
 
 projectRouter.post('/projects', authUser, createProject);
 
-projectRouter.put('/projects/:id', authUser, //to be implemented
-    );
+projectRouter.put('/projects/:id', authUser, updateProject);
 
-projectRouter.delete('/projects/:id', authUser, //to be implemented
-    );
+projectRouter.delete('/projects/:id', authUser, deleteProject);
 
 export default projectRouter;
